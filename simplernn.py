b764d05978ffd520b5848cd23832bf758cfb284f
MHuy3113 <96408274+MHuy3113@users.noreply.github.com>
2023-10-03 13:58:40 +0700

    Update tensorboard simplernn.py

--- simplernn.py
+++ simplernn.py
@@ -7,7 +7,15 @@ from torchvision.transforms import Compose, ToTensor, Normalize
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
-from utils import MNIST_loaders, save_model
+import os
+from utils import MNIST_loaders, save_model  # Make sure 'utils.py' is available with these functions
+
+# Create a directory to store the TensorBoard logs and model weights
+if not os.path.exists('runs'):
+    os.makedirs('runs')
+if not os.path.exists('weights'):
+    os.makedirs('weights')
+
 writer = SummaryWriter('runs/rnn_mnist')
 
 save_path = 'weights/rnn_mnist'
@@ -50,26 +58,25 @@ class RNN(nn.Module):
 # Khởi tạo mô hình và bộ tối ưu hóa
 model = RNN(input_size, hidden_size, num_layers, num_classes).to(device)
 loss_func = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr = learning_rate)
-   
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
 torch.manual_seed(1234)
-train_loader, test_loader = MNIST_loaders(batch_size,batch_size)
+train_loader, test_loader = MNIST_loaders(batch_size, batch_size)
 
-
-images, labels = next(iter(train_loader))
-grid = torchvision.utils.make_grid(images)
-writer.add_image('images', grid)
-images = images.reshape(-1, sequence_length, input_size).to(device)
-writer.add_graph(model,images)
-
-# Hàm tính độ chính xác
+# Function to calculate accuracy
 def get_accuracy(logit, target, batch_size):
     corrects = (torch.max(logit, 1)[1].view(target.size()).data == target.data).sum()
     accuracy = 100.0 * corrects / batch_size
     return accuracy.item()
 
-# Vòng lặp huấn luyện
+# Visualize images in TensorBoard
+images, labels = next(iter(train_loader))
+grid = torchvision.utils.make_grid(images)
+writer.add_image('images', grid)
+images = images.reshape(-1, sequence_length, input_size).to(device)
+writer.add_graph(model, images)
+
+# Training loop
 running_loss = 0
 best_loss = 0
 for epoch in range(num_epochs):
@@ -77,41 +84,40 @@ for epoch in range(num_epochs):
     model.train()
 
     for i, (images, labels) in enumerate(tqdm(train_loader), 0):
-        
         images = images.reshape(-1, sequence_length, input_size).to(device)
         labels = labels.to(device)
+
         # Forward pass
         outputs = model(images)
         loss = loss_func(outputs, labels)
-        
+
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        # train_running_loss += loss.detach().item()
-        # train_acc += get_accuracy(outputs, labels, batch_size)
         running_loss += loss.item()
+
     avg_loss = running_loss / len(train_loader)
-    writer.add_scalar('training loss',
-                    avg_loss,
-                    epoch)
-    
-    print ('Epoch [{}/{}],  Loss: {:.4f}, Best loss: {:.4f}' 
-                .format(epoch + 1, num_epochs, avg_loss, best_loss))
+    writer.add_scalar('training loss', avg_loss, epoch)
+
+    # Calculate accuracy
+    model.eval()
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in test_loader:
+            images = images.reshape(-1, sequence_length, input_size).to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total = total + labels.size(0)
+            correct = correct + (predicted == labels).sum().item()
+
+        accuracy = 100 * correct / total
+        writer.add_scalar('accuracy', accuracy, epoch)
+
+    print('Epoch [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%, Best loss: {:.4f}'.format(epoch + 1, num_epochs, avg_loss, accuracy, best_loss))
     best_loss = save_model(model, optimizer, epoch, best_loss, avg_loss, save_path)
     running_loss = 0.0
 
-# Test the model
-model.eval()
-with torch.no_grad():
-    correct = 0
-    total = 0
-    for images, labels in test_loader:
-        images = images.reshape(-1, sequence_length, input_size).to(device)
-        labels = labels.to(device)
-        outputs = model(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total = total + labels.size(0)
-        correct = correct + (predicted == labels).sum().item()
-print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
-
-
+# Close the TensorBoard writer
+writer.close()
